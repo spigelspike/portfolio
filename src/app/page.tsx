7c0b1d27259aaae1462da71c03cf8ef56a69c5ab
spigelspike <shareefch1413@gmail.com>
2026-06-26 04:11:57 +0530

    add custom favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
       <GameHUD />
       <main className="bg-[#050A1F] min-h-screen pb-10 w-full overflow-x-hidden">
         <SpawnArea />
-        
+
         {/* Dashboard Stack Container */}
         <div className="w-full px-2 sm:px-4 md:px-8 mt-6 flex flex-col gap-10">
           <AboutSection />
